3f082bcc305f7934ec80a5ffc3ebc157bb3a8277
GrittyCode <kanghan9830@gmail.com>
2022-12-04 19:47:25 +0900

    DirectX12 Study
    DirectX12 Study on 2022-12-04

---
+++ Client/Game.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Game.h"
+
+
+void Game::Init()
+{
+	HelloEngine();
+	vector<int> i;
+	
+}
+
+void Game::Update()
+{
+}
---
+++ Client/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+//전처리기가 코드를 읽어가지고 추가해주는 것(컴파일 이전단계에)
+#pragma comment(lib, "Engine.lib")
+
+
+#include "EnginePch.h"
+
+#include<vector>
+#include<memory>
+using namespace std;
